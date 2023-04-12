36f2b1ffd00d68b6633a4438ed412e1144d8aa58
Kitalfa <dorian.travers@yahoo.fr>
2023-04-12 15:37:00 +0200

    Setup middlewares

---
+++ index.js
@@ -0,0 +1,33 @@
+/* ---------- DotEnv ---------- */
+const dotenv = require('dotenv');
+dotenv.config();
+
+/* ---------- Express ---------- */
+
+const express = require('express');
+const router = require('./app/router');
+const cors = require('cors');
+const multer = require('multer');
+const bodyParser = multer();
+const middlewares = require('./app/middlewares');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+/* ---------- Middlewares ---------- */
+app.use(cors('*')); // On autorise tout les domaines à faire du Cross Origin Resource Sharing.
+app.use(bodyParser.none());
+app.use(middlewares.bodySanitizer);
+app.use(router);
+
+app.use(express.static('assets'));
+
+app.use(middlewares.notFoundMiddleware);
+
+/* ---------- App ---------- */
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT} ...`);
+});
